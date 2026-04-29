9a8eefd84a5640dccfac28d19575af2d8c14aaa9
Mabel Adekola <marble_oyeranti@yahoo.co.uk>
2026-04-29 13:31:14 -0600

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://meridiandisputeresolution.ca',
+  site: 'https://github.com/maybellyne/meridiandisputeresolution',
+  base: '/meridiandisputeresolution',
   output: 'static',
   trailingSlash: 'never',
-
-  integrations: [
-    sitemap(),
-  ],
 });
